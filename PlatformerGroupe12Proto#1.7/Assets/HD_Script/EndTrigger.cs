ca39d641a395e4805e55fbd96f78ca3930630f90
hugo D <demortierhugo@gmail.com>
2020-05-17 23:27:10 +0200

    LevelEND

---
+++ PlatformerGroupe12Proto#1.7/Assets/HD_Script/EndTrigger.cs
@@ -0,0 +1,11 @@
+﻿
+using UnityEngine;
+
+public class EndTrigger : MonoBehaviour
+{
+    public GameManage gameManage;
+   void OnTriggerEnter2D()
+    {
+        gameManage.CompleteLevel();
+    }
+}
---
+++ PlatformerGroupe12Proto#1.7/Assets/HD_Script/GameManage.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+
+public class GameManage : MonoBehaviour
+{
+    bool gameHasEnded = false;
+    public float restartDelay = 1f;
+
+    public void CompleteLevel()
+    {
+        Debug.Log("Level Won");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void EndGame()
+    {
+        if (gameHasEnded == false)
+        {
+            gameHasEnded = true;
+            Debug.Log("Game Over");
+            Invoke("Restart", restartDelay);
+        }
+    }
+    void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+
+}
+
