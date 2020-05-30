36dad888bdd963e7343ef459898c9798f1dacca8
Ulric <ulric.debaert@outlook.fr>
2020-05-30 15:20:56 +0200

    MAJ Input

--- PlatformerGroupe12Proto#1.7/Assets/HD_Script/PauseMenu.cs
+++ PlatformerGroupe12Proto#1.7/Assets/HD_Script/PauseMenu.cs
@@ -15,7 +15,7 @@ public class PauseMenu : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
+        if (Input.GetKeyDown(KeyCode.Escape) || Input.GetButtonUp("Start"))
         {
 
             if (GameIsPaused)
